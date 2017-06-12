5aa4a39baf05c4fd11e4bc66b3bcfffcf1da0525
Unknown <tinquenl2@gmail.com>
2017-06-12 16:42:00 +0200

    nav

--- htdocs/aPropos.php
+++ htdocs/aPropos.php
@@ -18,10 +18,10 @@
 <header>
       <a href="index.php"><img id="logoAile" alt="Aile avec le logo dedans" src="images/imgLogoOmbre.png"></a>        
         <nav>
-            <a class="policeNav ongletSelect" href="index.php">ACCUEIL</a>
+            <a class="policeNav" href="index.php">ACCUEIL</a>
             <a class="policeNav"  href="reglement.php">REGLEMENT</a>
             <a class="policeNav" href="publication.php">PUBLICATIONS</a>
-            <a class="policeNav" href="aPropos.php">A PROPOS</a>
+            <a class="policeNav ongletSelect" href="aPropos.php">A PROPOS</a>
             <a class="policeNav" id="participer" href="participer.php">PARTICIPER</a>
             <a class="policeNav" id="btnCo" style="cursor:pointer">Se connecter</a>
         </nav>
