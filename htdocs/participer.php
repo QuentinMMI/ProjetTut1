667ca618fd1aa50fa58c1ad5d970a3374802c78d
Unknown <thomas.furiet@gmail.com>
2017-06-14 17:24:27 +0200

    Participer

--- htdocs/participer.php
+++ htdocs/participer.php
@@ -76,64 +76,11 @@ session_start();
             </div>
         </div>
 </header>
-<body>
-<header>
-        <a href="index.php"><img id="logoAile" alt="Aile avec le logo dedans" src="images/imgLogoOmbre.png"></a>        
-        <nav>
-            <a class="policeNav" href="index.php">ACCUEIL</a>
-            <a class="policeNav"  href="reglement.php">REGLEMENT</a>
-            <a class="policeNav" href="publication.php">PUBLICATIONS</a>
-            <a class="policeNav" href="aPropos.php">A PROPOS</a>
-            <a class="policeNav  ongletSelect" id="participer" href="participer.php">PARTICIPER</a>
-            <a class="policeNav" id="btnCo" style="cursor:pointer">Se connecter</a>
-        </nav>
-        <div id="Modal" class="modal">
-            <div class="modal-content">
-                <div id="divClose">
-                <a id="close" style="cursor:pointer">fermer</a>
-                </div>
-                <div id="connexion">
-                    <h1>ConnExiON</h1>
-                    <form action="" method="get">
-                        <label for="mailC">Mail</label>
-                        <input type="text" id="mailC" name="mailC" required="required">
-                        <label for="mdpC">Mot De Passe</label>
-                        <input type="text" id="mdpC" name="mdpC" required="required">
-                        <input type="submit" value="OK !" id="submitC" name="submit" style="cursor:pointer"/>
-                    </form>
-                </div>
-                <div id="inscrip">
-                    <form action="" method="get">
-                        <label for="prenomI">Prénom</label>
-                        <input type="text" id="prenomI" name="prenomI" required="required">
-                        <label for="nomI">Nom</label>
-                        <input type="text" id="nomI" name="nomI" required="required">
-                        <label for="mailI">Mail</label>
-                        <input type="email" id="mailI" name="mailI" required="required">
-                        <label for="mdpI">Mot De Passe</label>
-                        <input type="password" id="mdpI" name="mdpI" required="required">
-                        <label for="mdpIc">Cofirmer</label>
-                        <input type="password" id="mdpIc" name="mdpIc" required="required">
-                        <label for="dateI">Date de naissance</label>
-                        <input type="date" id="dateI" name="dateI" required="required" value="">
-                        <input type="checkbox" id="CGU" name="CGU" value="1">
-                        <label for="CGU">Accepter le <a href="mentionsLegales.php">CGU</a></label>
-                        <input type="submit" value="Valider" id="submit" name="submit" style="cursor:pointer"/>
-                    </form>
-                    <h1>S'InsCrirE</h1>
-                </div>
-            </div>
-        </div>
-    </header>
+
     <main id="mainPublier">
-        <div id="div1Publier" class="divPubliser">
-            
-        </div>
-        <div id="div2Publier" class="divPubliser">
-            
-        </div>
-        <div id="div3Publier" class="divPubliser">
-            
+        <div id="planche1" class="planche planche1">
+            <h1 id="publications">Mes publications</h1>
+            <p>Pour participer, choisis au moins un type de fichier et dépose le ci-dessous. Tu peux participer une seule fois sans chaque catégorie, si tu souhaites modifier ton travail tu es libre de le faire jusqu'à la date limite du concours.</p>
         </div>
     </main>
     
