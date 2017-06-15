8bc398950a613198df268026b3a44e59ea259049
Unknown <tinquenl2@gmail.com>
2017-06-15 09:58:22 +0200

    page règlement

--- htdocs/reglement.php
+++ htdocs/reglement.php
@@ -17,46 +17,36 @@ session_start();
 <header>
       <?php require("php/header.php"); ?>
 </header>
-<main class="fondJaune" id="fondReglement">
-    <h1 class="titreReglement"> Les Regles Du Concours</h1>
-    <p id="texteLibreDroit">Vous avez 16 ans ou plus, votre production est libre de droit commercial et doit être réalisée à partir de documents vous appartenant ou étant libres de droits. </p>
-    <div class="LigneUnRegle" >
-        <div class="ViSonPass">
-            <h2>Video</h2>
-            <p>1 à 3 min max</p>
-            <p>Bande son obligatoire</p>
-            <p>Une qualité HD 720p minimum</p>
-            <p>Uploader sur Youtube en non répértoriée </p>
-        </div>
-        
-        <div>
-            <img id="passeport" src="images/imgPasseport.png" alt="Image de passeport" >
-        </div>
-        
-        <div class="ViSonPass">
-            <h2>Musique/son</h2>
-            <p>1 à 3 min max</p>
-            <p>mp3</p>
-            <p>Une qualité son optimal en stéréo</p>
-            <p>Uploader sur SoundCloud en privé</p>
-        </div>
-        </div>
-        <div id="regleAffiche">
-            <h2>Affiche</h2>
-            <p>Un poid maximum de 750 Ko</p>
-            <p>Format 2500 X 3000 minimum</p>
-        </div>
-        
-        <div id="regleSon">
-            <img src="images/imgLivre.png" alt="Image de livre" >
-        </div>    
-        
-        <div>
-            <p id="ilNeVousReste"> Il ne vous reste plus qu'à participer!</p>
-        </div>
-    
-</main>
-
+<main class="fondJaune" id="fondReglement"> 
+            <h1 class="titreReglement"> Les Regles Du Concours</h1> 
+            <div class="ligneUnRegle"> 
+                <p id="texteLibreDroit">Vous devez avoir 16 ans ou plus et votre production ainsi que les documents utilisés doivent être libres de droit commercial.</p> 
+            </div> 
+            <div id="videoRegle"> 
+                <h2>Video</h2> 
+                <p>1 à 3 min max</p> 
+                <p>Bande son obligatoire</p> 
+                <p>Une qualité HD 720p minimum</p> 
+                <p>Uploader sur Youtube en non répértoriée </p> 
+            </div>
+            <div id="afficheRegle"> 
+                <h2>Affiche</h2> 
+                <p>Un poid maximum de 750 Ko</p> 
+                <p>Format 2500 X 3000 minimum</p> 
+            </div> 
+            <div id="sonRegle"> 
+                <h2>Musique/son</h2> 
+                <p>1 à 3 min max</p> 
+                <p>mp3</p> 
+                <p>Une qualité son optimal en stéréo</p> 
+                <p>Uploader sur SoundCloud en privé</p> 
+            </div> 
+            <div id="regleSon"> 
+                <div> 
+                    <p id="ilNeVousReste"> Il ne vous reste plus qu'a participer!</p> 
+                </div> 
+            </div> 
+        </main> 
 <footer>
     <?php require("php/footer.php"); ?>
 </footer>
