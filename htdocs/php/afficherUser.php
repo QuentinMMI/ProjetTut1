cbc4cd36ac1236c19b1905acfa3fb20732a525ac
Unknown <fesselier.hugo@laposte.net>
2017-06-16 15:49:40 +0200

    debut ajax

--- htdocs/php/afficherUser.php
+++ htdocs/php/afficherUser.php
@@ -1,3 +1,16 @@
-<?php
-    echo("coucou c'est moi");
+<?php 
+    session_start();
+    require("param.inc.php") ;
+    $bdd=new PDO("mysql:host=".MYHOST.";dbname=".MYDB, MYUSER, MYPASS) ;
+    
+
+    $verif = $bdd -> prepare("SELECT NomUser, PrenomUser, DateNaissance, AdresseMail FROM UTILISATEUR WHERE IdUser = :User");
+    $verif->execute(array(":User" => $_GET['idOeuvre']));
+    
+    $profil= $verif->fetch(PDO::FETCH_ASSOC);
+
+    
+	
+    echo($signalement->nbSignalementOeuvre);
+
 ?>
