8ea2a39c10dfe5808f267f899127934ca1eab070
Unknown <thomas.furiet@gmail.com>
2017-06-19 15:19:32 +0200

    Poster avec Quentin

--- htdocs/php/depotImage.php
+++ htdocs/php/depotImage.php
@@ -1,3 +1,23 @@
 <?php
-	 header ("Content-type: text/html");
+    session_start();
+    require("param.inc.php");
+if (isset($_FILES["affiche"])){
+            if($_FILES["affiche"]["name"] != ".htaccess"){
+                if($_FILES["affiche"]["type"] == "image/jpeg"|| $_FILES["affiche"]["type"] == "image/pjpeg"){
+                    //$_SERVEUR
+                    //$_GET
+                    //$_POST
+                    echo("Nom du fichier : ".$_FILES["affiche"]["name"]."<br />"); 
+                    echo("Nom du fichier temporaire : ".$_FILES["affiche"]["tmp_name"]."<br />"); 
+                    echo("Type du fichier : ".$_FILES["affiche"]["type"]."<br />");  
+                    echo("Taille du fichier : ".$_FILES["affiche"]["size"]."  octets<br />"); 
+                    
+                    copy($_FILES["affiche"]["tmp_name"],"../travaux/".$_FILES["affiche"]["name"]);
+                    require("php/convertirImage85x85.inc.php");
+                    convertirImage85x85($_FILES["affiche"]["tmp_name"],"../travaux/vignette/vignette_".$_FILES["affiche"]["name"]);
+                }
+            }
+}else{
+    header("Location: poster.php");
+}
 ?>
