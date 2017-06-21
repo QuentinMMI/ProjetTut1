64e45cb35df28d93e54903c2844a7aad4e51f0a4
Unknown <fesselier.hugo@laposte.net>
2017-06-21 09:52:59 +0200

    MAJ
    empeche la supression du SU :-D

--- htdocs/php/AJAX-php/supprimeUser.php
+++ htdocs/php/AJAX-php/supprimeUser.php
@@ -6,12 +6,14 @@
     $pdo->query("SET NAMES utf8");
     $pdo->query("SET CHARACTER SET 'utf8'");
 
-
+    if($_GET['id']==1){
+        $codeHTML = 'Vous ne pouvez pas supprimer le compte administrateur.';
+    }else{
     //requet sql
         $sql = $pdo -> prepare ("DELETE FROM UTILISATEUR WHERE IdUser = :Id ;");
         $sql->execute(array(":Id" => $_GET['id']));
         $codeHTML='profil supprimé';
-
+    }
 
 $pdo=null;
 
