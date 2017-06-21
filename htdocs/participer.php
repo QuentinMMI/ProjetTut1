cf85e54f9ba935a3f804e8674143bb65abaa3db8
Unknown <tinquenl2@gmail.com>
2017-06-21 14:48:15 +0200

    redirection travail

--- htdocs/participer.php
+++ htdocs/participer.php
@@ -1,6 +1,27 @@
 <?php
 session_start();
     header("Content-type: text/html; charset=UTF-8");
+    require("php/param.inc.php");
+    if(isset($_SESSION["id"])){
+        $bdd =new PDO("mysql:host=".MYHOST.";dbname=".MYDB, MYUSER, MYPASS) ;
+        $insertmbr = $bdd->prepare("SELECT * FROM A_Publie WHERE IdUser=:id");
+        $insertmbr->execute(array(":id"=>$_SESSION["id"]));
+        $data = $insertmbr->fetch();
+        if($data["IdPhoto"]!=0){
+            $inserta = $bdd->prepare("SELECT AccesMiniature FROM PHOTO WHERE IdPhoto=:id");
+            $inserta->execute(array(":id"=>$data["IdPhoto"]));
+            $dataA = $inserta->fetch();
+        }
+        if($data["IdSon"]!=0){
+            $inserts = $bdd->prepare("SELECT AccesMiniature FROM SON WHERE IdSon=:id");
+            $inserts->execute(array(":id"=>$data["IdSon"]));
+            $dataS = $inserts->fetch();
+        }
+        if($data["IdVideo"]!=0){
+            $insertv = $bdd->prepare("SELECT AccesMiniature FROM VIDEO WHERE IdVideo=:id");
+            $insertv->execute(array(":id"=>$data["IdVideo"]));
+            $dataV = $insertv->fetch();
+        }
 ?>
 <html>  
 <head>
@@ -27,17 +48,23 @@ session_start();
             <div id="lesIcones">  
                 <div class="divAffiche">  
                     <img class="modifier" src="images/imgModifier.png" alt="image modifier">  
-                    <a href="poster.php?type=affiche" ><img class="depot" src="images/imgIconeAffiche.png" alt="image de l'affiche"></a>
+                    <a href="<?php if($data["IdPhoto"]==0){ ?>poster.php?type=affiche<?php }else{ ?>afficher.php?type=affiche&id=<?php echo $data["IdPhoto"]; }; ?>" >
+                        <img class="depot" src="<?php if($data["IdPhoto"]==0){ ?>travaux/vignette/img1.png<?php }else{echo $dataA["AccesMiniature"];}; ?>" alt="image de l'affiche">
+                    </a>
                     <img class="modifier" src="images/imgPoubelle.png" alt="image poubelle">  
                 </div>  
                 <div class="divAffiche">  
                     <img class="modifier" src="images/imgModifier.png" alt="image modifier">  
-                    <a href="poster.php?type=son"><img class="depot" src="images/imgIconeSon.png" alt="image du son"></a>  
+                    <a href="<?php if($data["IdSon"]==0){ ?>poster.php?type=son<?php }else{ ?>afficher.php?type=son&id=<?php echo $data["IdSon"]; }; ?>" >
+                        <img class="depot" src="<?php if($data["IdSon"]==0){ ?>travaux/vignette/son1.png<?php }else{echo $dataS["AccesMiniature"];}; ?>" alt="image de l'affiche">
+                    </a>  
                     <img class="modifier" src="images/imgPoubelle.png" alt="image poubelle">  
                 </div>  
                 <div class="divAffiche">  
                     <img class="modifier" src="images/imgModifier.png" alt="image modifier">  
-                    <a href="poster.php?type=video"><img class="depot" src="images/imgIconeVideo.png" alt="image de la vidéo"></a>
+                    <a href="<?php if($data["IdVideo"]==0){ ?>poster.php?type=video<?php }else{ ?>afficher.php?type=video&id=<?php echo $data["IdVideo"]; }; ?>" >
+                        <img class="depot" src="<?php if($data["IdVideo"]==0){ ?>travaux/vignette/vid1.png<?php }else{echo $dataV["AccesMiniature"];}; ?>" alt="image de l'affiche">
+                    </a>
                     <img class="modifier" src="images/imgPoubelle.png" alt="image poubelle">  
                 </div>  
             </div> 
@@ -51,3 +78,8 @@ session_start();
     <script src="java/verif.js"></script>
 </body>
 </html>
+<?php
+    }else{
+        header("Location: index.php");
+    }
+    ?>
