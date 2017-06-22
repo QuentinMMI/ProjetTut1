f43b1e09f23b977d15e23b7dd7c38be8dfa680d1
Unknown <fesselier.hugo@laposte.net>
2017-06-22 16:11:45 +0200

    MAJ admin
     suppression du dossier travaux lorsqu'on supprime un profil

--- htdocs/php/AJAX-php/supprimeUser.php
+++ htdocs/php/AJAX-php/supprimeUser.php
@@ -21,12 +21,24 @@
         
         //SUPPRESSION DES TABLE SON VIDEO ET PHOTO
         if($oeuvre['IdSon'] != 0){
+            $sql = "SELECT AccesSon FROM SON WHERE IdSon =".$_GET['id'];
+            $statement = $pdo->query($sql);
+
+            $oeuvre = $statement->fetch(PDO::FETCH_ASSOC); 
+            unlink ("../../".$oeuvre['AccesSon']);
+            
             //requet sql
             $sql = $pdo -> prepare ("DELETE FROM SON WHERE IdSon = :Id;");
             $sql->execute(array(":Id" => $oeuvre['IdSon']));
         }
         
         if ($oeuvre['IdVideo'] != 0){
+            $sql = "SELECT AccesVideo FROM VIDEO WHERE IdVideo =".$_GET['id'];
+            $statement = $pdo->query($sql);
+
+            $oeuvre = $statement->fetch(PDO::FETCH_ASSOC); 
+            unlink ("../../".$oeuvre['AccesVideo']);
+            
             //requet sql
             $sql = $pdo -> prepare ("DELETE FROM VIDEO WHERE IdVideo = :Id ;");
             $sql->execute(array(":Id" => $oeuvre['IdVideo']));
@@ -34,6 +46,12 @@
         
         
         if ($oeuvre['IdPhoto'] != 0){
+            $sql = "SELECT AccesPhoto FROM PHOTO WHERE IdPhoto =".$_GET['id'];
+            $statement = $pdo->query($sql);
+
+            $oeuvre = $statement->fetch(PDO::FETCH_ASSOC); 
+            unlink ("../../".$oeuvre['AccesPhoto']);
+            
             //requet sql
             $sql = $pdo -> prepare ("DELETE FROM PHOTO WHERE IdPhoto = :Id ;");
             $sql->execute(array(":Id" => $oeuvre['IdPhoto']));
